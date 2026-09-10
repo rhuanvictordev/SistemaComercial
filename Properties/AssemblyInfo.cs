74ad7532bcabb86dbf38978f67f7084c3f60f750
Rhuan Victor <rhuansoliveira1072@gmai.com>
2026-09-09 22:56:55 -0300

    icone

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SistemaComercial")]
+[assembly: AssemblyTitle("Sistema")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SistemaComercial")]
+[assembly: AssemblyProduct("Sistema")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
